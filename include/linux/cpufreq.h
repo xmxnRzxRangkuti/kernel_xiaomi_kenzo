d1e5329bbafcf10a709cfd3cb00dbce7535c0c1b
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-09-19 08:00:03 +0000

    [SQUASH] Revert some unused cpu governor
    Revert "cpufreq: add Cafactive gov - modded interactive from code aurora"
    
    This reverts commit 8bc6897ec7b8d40d772211b27bd96b63a25dff4c.
    
    Revert "Add Impulse"
    
    This reverts commit b5d69e91c5e633fcde19ce1b66c0a8f8aba2b503.
    
    Revert "add bioshock governer"
    
    This reverts commit 604d1a7069436ba239c2bb03947bfb46c62ccee8.
    
    Revert "cpufreq:add zzmove profiles"
    
    This reverts commit 49fc3c75bbb7a8be129620dcd7abe62e25b7f2ad.
    
    Revert "cpufreq: ondemand: update code to Linux v3.18"
    
    This reverts commit 94885a0f0d418076864fa40102af0906d6287280.
    
    Revert "cpufreq/zzmoove: updated to bLE develop version as of 11.07.17"
    
    This reverts commit dbc7c003427cea30bcd3321cfe116178291ff2e2.
    
    Revert "cpufreq: add ZZMoove big.LITTLE Edition (bLE) development version as of 09.01.2016"
    
    This reverts commit bacd46ba5b213e3276f36069dbaa1f4b9f571e18.
    
    Revert "defconfig: Update defconfig"
    
    This reverts commit b5913eeb71f90d5bd2829dd5e333a31ab812a4ca.
    
    Revert "cpufreq; add elementalx governor"
    
    This reverts commit 375d26991d4e4b475975d588e883f3cada99150b.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -417,13 +417,6 @@ void cpufreq_unregister_governor(struct cpufreq_governor *governor);
 #ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
 extern struct cpufreq_governor cpufreq_gov_performance;
 #endif
-#ifdef CONFIG_CPU_FREQ_GOV_INTERACTIVE
-extern unsigned int cpufreq_interactive_get_hispeed_freq(int cpu);
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_CAFACTIVE
-extern unsigned int cpufreq_cafactive_get_hispeed_freq(int cpu);
-extern void cafactive_boost_ondemand(int cpu, s64 miliseconds, bool static_switch);
-#endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
@@ -441,22 +434,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
-extern struct cpufreq_governor cpufreq_gov_zzmoove;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
-extern struct cpufreq_governor cpufreq_gov_bioshock;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
-extern struct cpufreq_governor cpufreq_gov_impulse;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
-extern struct cpufreq_governor cpufreq_gov_elementalx;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
-extern struct cpufreq_governor cpufreq_gov_cafactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
-
 #endif
 
 /*********************************************************************
